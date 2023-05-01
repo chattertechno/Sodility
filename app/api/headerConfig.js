bdb4dfc880aac9bf6b1c847675897d6a0ac65552
Emmanuel E <49331376+TalentedSunday@users.noreply.github.com>
2023-05-02 01:29:55 +0530

    Fixes in API integration from Hariom (#7)
    * isses fixed code merge
    
    * fixed bug from merge
    
    ---------
    
    Co-authored-by: TalentedSunday <>

--- app/api/headerConfig.js
+++ app/api/headerConfig.js
@@ -1,7 +1,8 @@
 // import {Token} from "./token";
 //const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImhhcmlvbSIsImRhc2giOiJkZmFzZGYzMnN3c3dyZmFmZGZkd2VlIiwidXNlcl9pZCI6IjY0NGY3ZDg3MjIwNWZmZDU0ODhjYjI1ZiIsInJvbGUiOiJjcmVhdGVyIiwiZXhwIjoxNjgzMDE4OTIwfQ.9CFfPTLAGcdrqT23t5SOMErf5EHkXgx_D04BUlbBqIk';
+import { getLocaleData } from "../../service/authService";
 
-const TOKEN = localStorage.getItem("token");
+const TOKEN = getLocaleData("token");
 const HeaderAPI = () => {
   return { Accept: "application/json", "Content-Type": "application/json", Authorization: `Bearer ${TOKEN}` };
 };
